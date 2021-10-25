cb6a51fda3688f05e33a3b0dd8c838b2025aa069
2efeng <h-2efeng@qq.com>
2021-10-26 00:47:32 +0800

    fix

--- src/main/java/com/hzf/mybatisdemo/config/DbSourceConfig.java
+++ src/main/java/com/hzf/mybatisdemo/config/DbSourceConfig.java
@@ -1,12 +1,10 @@
 package com.hzf.mybatisdemo.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
